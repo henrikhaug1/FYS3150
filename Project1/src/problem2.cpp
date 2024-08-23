1715b28d2b5c2a26cc31c05e6a64e367eb5d0c27
Henrik <heh@uio.no>
2024-08-23 15:42:13 +0200

    moved main function in problem2.cpp to main.cpp

--- Project1/src/problem2.cpp
+++ Project1/src/problem2.cpp
@@ -36,12 +36,3 @@ void write_to_file(std::string filename, arma::vec x_vec, arma::vec ux_vec) {
     }
     ofile.close();
 }
-
-int main() {
-
-arma::vec x_vec = fill_x_vec(0, 1, 100);
-arma::vec ux_vec = fill_ux_vec(x_vec);
-write_to_file("x_and_ux_values_output.txt", x_vec, ux_vec);
-
-	return 0;
-}
