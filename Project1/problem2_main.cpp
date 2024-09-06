47753e66d5aa2e5a8d011dd5f40ae94984fa9da7
Henrik <heh@uio.no>
2024-09-06 08:57:37 +0200

    added main files to repository

---
+++ Project1/problem2_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "problem2_functions.hpp"
+
+int main() {
+
+arma::vec x_vec = fill_x_vec(0, 1, 100);
+arma::vec ux_vec = fill_ux_vec(x_vec);
+write_to_file("x_and_ux_values_output.txt", x_vec, ux_vec);
+
+    return 0;
+}
