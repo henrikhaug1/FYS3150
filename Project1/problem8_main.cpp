6426b155520cb8adb875a5f8c5b84e398a2f2e4f
livelst <livelst@uio.no>
2024-09-07 13:05:02 +0200

    continued on problem8_functions.cpp

--- Project1/problem8_main.cpp
+++ Project1/problem8_main.cpp
@@ -1,5 +1,33 @@
+#include "problem2_functions.hpp"
+#include "problem7_functions.hpp"
 #include "problem8_functions.hpp"
 
 int main(){
+
+	// finding abolsute- and relative error for n_step = 10
+	arma::mat v_x_mat_10 = thomas_algo(10, -1, 2, -1);
+	arma::vec approx_vec_10 = v_x_mat_10.col(0);
+	arma::vec true_vec_10 = fill_ux_vec(fill_x_vec(0, 1, 10));
+
+	arma::vec abs_10 = absolute_error(approx_vec_10, true_vec_10);
+	arma::vec rel_10 = relative_error(approx_vec_10, true_vec_10);
+
+	// finding abolsute- and relative error for n_step = 100
+	arma::mat v_x_mat_100 = thomas_algo(100, -1, 2, -1);
+	arma::vec approx_vec_100 = v_x_mat_10.col(0);
+	arma::vec true_vec_100 = fill_ux_vec(fill_x_vec(0, 1, 100));
+
+	arma::vec abs_100 = absolute_error(approx_vec_100, true_vec_100);
+	arma::vec rel_100 = relative_error(approx_vec_100, true_vec_100);
+
+	// finding abolsute- and relative error for n_step = 1000
+	arma::mat v_x_mat_1000 = thomas_algo(1000, -1, 2, -1);
+	arma::vec approx_vec_1000 = v_x_mat_10.col(0);
+	arma::vec true_vec_1000 = fill_ux_vec(fill_x_vec(0, 1, 1000));
+
+	arma::vec abs_1000 = absolute_error(approx_vec_1000, true_vec_1000);
+	arma::vec rel_1000 = relative_error(approx_vec_1000, true_vec_1000);
+
+
 	return 0;
 }
--- Project1/src/problem8_functions.cpp
+++ Project1/src/problem8_functions.cpp
@@ -1,13 +1,17 @@
-#include <iosstream>
 #include "problem8_functions.hpp"
+
+#include <armadillo>
+#include <iostream>
+#include <cmath>
 
 arma::vec absolute_error(arma::vec approx_vec, arma::vec true_vec){
 
 	std::size_t length_vec = approx_vec.size();
 
-	arma::vec absolute_error = arma::vec(length_vec);
+	arma::vec absolute_error_vec = arma::vec(length_vec);
 	for(int i = 0; i < length_vec; i++){
-		absolute_error[i] = std::abs(approx_vec[i] - true_vec[i]);
+		double abs_value = fabs(approx_vec[i] - true_vec[i]);
+		absolute_error_vec[i] = fabs(abs_value);
 	}
 
 	return absolute_error_vec;
@@ -17,9 +21,10 @@ arma::vec relative_error(arma::vec approx_vec, arma::vec true_vec){
 
 	std::size_t length_vec = approx_vec.size();
 
-	arma::vec relative_error = arma::vec(length_vec);
+	arma::vec relative_error_vec = arma::vec(length_vec);
 	for(int i = 0; i < length_vec; i++){
-		relative_error[i] = std::abs((approx_vec[i] - true_vec[i]) / true_vec);
+		double rel_value = (approx_vec[i] - true_vec[i]) / true_vec[i];
+		relative_error_vec[i] = fabs(rel_value);
 	}
 
 	return relative_error_vec;
