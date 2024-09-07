68703611fe9f8aa59e7996e41967a84848317425
livelst <livelst@uio.no>
2024-09-07 12:22:07 +0200

    implemented functions absolute_error and relative_error for problem 8

--- Project1/include/problem8_function.hpp
+++ Project1/include/problem8_function.hpp
@@ -1,5 +1,11 @@
 #ifndef __problem8_functions_hpp__
 #define __problem8_functions_hpp__
 
+#include <armadillo>
+#include <iostream>
+
+arma::vec absolute_error(arma::vec approx_vec, arma::vec true_vec);
+
+arma::vec relative_error(arma::vec approx_vec, arma::vec true_vec);
 
 #endif
--- Project1/src/problem8_functions.cpp
+++ Project1/src/problem8_functions.cpp
@@ -1,2 +1,30 @@
 #include <iosstream>
 #include "problem8_functions.hpp"
+
+arma::vec absolute_error(arma::vec approx_vec, arma::vec true_vec){
+
+	std::size_t length_vec = approx_vec.size();
+
+	arma::vec absolute_error = arma::vec(length_vec);
+	for(int i = 0; i < length_vec; i++){
+		absolute_error[i] = std::abs(approx_vec[i] - true_vec[i]);
+	}
+
+	return absolute_error_vec;
+}
+
+arma::vec relative_error(arma::vec approx_vec, arma::vec true_vec){
+
+	std::size_t length_vec = approx_vec.size();
+
+	arma::vec relative_error = arma::vec(length_vec);
+	for(int i = 0; i < length_vec; i++){
+		relative_error[i] = std::abs((approx_vec[i] - true_vec[i]) / true_vec);
+	}
+
+	return relative_error_vec;
+}
+
+
+
+
