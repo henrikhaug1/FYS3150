ae0963abb09eee76c5586307ae4d33033ebdaad6
Henrik <heh@uio.no>
2024-09-07 16:45:22 +0200

    finished plotting functoin for problem 8

--- Project1/src/problem8_plotting.py
+++ Project1/src/problem8_plotting.py
@@ -1,40 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_problem8(path_absolute, path_relative, n_step):
-	absolute_error_values = np.zeros(n_step)
-	relative_error_values = np.zeros(n_step)
-	xi_values = np.linspace(0, 1, n_step)
+def plot_problem8(error_type, n_step_list):
 
+	plot = plt.figure(figsize=(12, 8))
 
-	with open(path_absolute, "r") as infile:
-		absolute_error_list = []
-		for line in infile:
-			absolute_error_list.append(line.split()[0])
-			
+	for i in range(len(n_step_list)):
+		with open("../" + str(error_type) + f"_error{n_step_list[i]}.txt", "r") as infile:
 
-		for i in range(0, n_step):
-			absolute_error_values[i] = float(absolute_error_list[i])
-			
+			error_values = np.zeros(n_step_list[i])
+			xi_values = np.linspace(0, 1, n_step_list[i])
 
-	with open(path_relative, "r") as infile:
-		relative_error_list = []
-		for line in infile:
-			relative_error_list.append(line.split()[0])
-		for i in range(0, n_step):
-			relative_error_values[i] = float(relative_error_list[i])
+			error_list = []
+			for line in infile:
+				error_list.append(line.split()[0])
+			for j in range(0, n_step_list[i]):
+				error_values[j] = float(error_list[j])
 
-	plt.figure(figsize=(12, 8))
-	plt.loglog(absolute_error_values, xi_values, 'o-', label='absolute_error_values')
-	plt.loglog(relative_error_values, xi_values, 'x--', label='Relative_error_values')
+			plt.loglog(error_values, xi_values, label = f"error_values_{n_step_list[i]}")
+
 
 	plt.xlabel('xi-values')
 	plt.ylabel('log_10(errors)')
-	plt.title(f'plot of log_10(errors) against x_i with n_steps = {n_step}')
+	plt.title(f'plot of log_10(errors) against x_i with n_steps = {n_step_list[i]}')
 	plt.legend()
 	plt.grid(True)
 	plt.show()
-	"""plt.savefig(f"plot_problem7_with_{n_step}_steps.pdf")"""
 
+n_step_list_absolute = [10, 100, 1000]
+error_type_absolute = "absolute"
+plot_problem8(error_type_absolute, n_step_list_absolute)
 
-plot_problem8("/Users/henrikhaug/Documents/Matematikk_med_informatikk/Semester5/FYS3150/Prosjekter_livelst/FYS3150/Project1/absolute_error10.txt", "/Users/henrikhaug/Documents/Matematikk_med_informatikk/Semester5/FYS3150/Prosjekter_livelst/FYS3150/Project1/relative_error10.txt", 10)
+n_step_list_relative = [10, 100, 1000]
+error_type_relative = "absolute"
+plot_problem8(error_type_relative, n_step_list_relative)
