d7561d7177896bb79cfc048850bcedd9bb97ccf6
livelst <livelst@uio.no>
2024-09-07 11:37:35 +0200

    fixed comment in problem7_functions.hpp

--- Project1/include/problem7_functions.hpp
+++ Project1/include/problem7_functions.hpp
@@ -8,7 +8,7 @@
 //Algorithm that solves the thomas algorithm
 arma::mat thomas_algo(int n_step, int subdiagonal, int diagonal, int superdiagonal);
 
-// function that writes vectors v and g to file
+// function that writes vectors v and x to file
 void write_thomas_to_file(std::string filename, arma::mat v_g_mat);
 
 #endif
