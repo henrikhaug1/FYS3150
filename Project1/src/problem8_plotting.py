a2ca441c465b86a213672909abb5e4ac4cc4378c
Henrik <heh@uio.no>
2024-09-08 14:59:07 +0200

    completed problem8 plotting

--- Project1/src/problem8_plotting.py
+++ Project1/src/problem8_plotting.py
@@ -1,23 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_problem8(filename):
+def plot_problem8(type_error, filename_list):
 
-	for i in n_step_list:	
-
+	for i in range(len(filename_list)):
 		h_values = []
 		absolute_values = []
 		relative_values = []
-		with open(filename, "r") as infile:
-			for line in infile():
-				infile.readline()
+		with open(filename_list[i], "r") as infile:
+			infile.readline()
+			for line in infile:
+				h_values.append(float(line.split()[0]))
+				absolute_values.append(float(line.split()[1]))
+				relative_values.append(float(line.split()[2]))
 
-				h_values.append(line.split[0])
-				absolute_values.append(line.split[1])
-				relative_values.append(line.split[2])
+		if type_error == "absolute":
+			plt.plot(h_values, np.log10(absolute_values))
 
-		plt.plot(h_values, np.log10(absolute_values))
-		plt.plot(h_values, np.log10(relative_values))
+		elif type_error == "relative":
+			plt.plot(h_values, np.log10(relative_values))
+		else:
+			print("type_error has to be relative or absolute")
+
+
+		
+		
+
 	plt.show()
 
-plot_problem8("h-abs-rel.txt")
+filename_list = ["h-abs-rel10.txt", "h-abs-rel100.txt", "h-abs-rel1000.txt"]
+plot_problem8("absolute", filename_list)
