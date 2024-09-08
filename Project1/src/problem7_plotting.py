679b85a902f288d5a9c8dac617a311d315686c26
Henrik <heh@uio.no>
2024-09-08 16:17:18 +0200

    corrected problem7_plotting.py

--- Project1/src/problem7_plotting.py
+++ Project1/src/problem7_plotting.py
@@ -5,41 +5,48 @@ def u(x):
 	func = (1 - ((1 - np.exp(-10)) * x) - np.exp(-10 * x))
 	return func
 
-def plot_problem7(path, n_step):
-	v_values = np.zeros(n_step)
-	x_values = np.zeros(n_step)
-	ux_values = np.zeros(n_step)
-
-
-	with open(path, "r") as infile:
-		v_list = []
-		x_list = []
-		for _ in range(7):
-			infile.readline()
-		for line in infile:
-			v_list.append(line.split()[0])
-			x_list.append(line.split()[1])
-
-		for i in range(0, n_step):
-			v_values[i] = float(v_list[i])
-			x_values[i] = float(x_list[i])
-			ux_values[i] = u(x_values[i])
-
-
+def plot_problem7(filename_list, n_step_list):
 	plt.figure(figsize=(12, 8))
-	plt.plot(x_values, v_values, 'o-', label='Numerical v-values')
-	plt.plot(x_values, ux_values, 'x--', label='Exact u(x)')
 
-	plt.xlabel('x-values')
-	plt.ylabel('Values')
-	plt.title(f'Comparison of Numerical Solution and Exact Solution with n_steps = {n_step}')
+
+	for i in range(len(n_step_list)):
+		v_values = np.zeros(n_step_list[i])
+		x_values = np.zeros(n_step_list[i])
+		ux_values = np.zeros(n_step_list[i])
+
+
+		with open(filename_list[i], "r") as infile:
+			v_list = []
+			x_list = []
+			for _ in range(7):
+				infile.readline()
+			for line in infile:
+				v_list.append(line.split()[0])
+				x_list.append(line.split()[1])
+
+			for j in range(n_step_list[i]):
+				v_values[j] = float(v_list[j])
+				x_values[j] = float(x_list[j])
+				ux_values[j] = u(x_values[j])
+
+			plt.plot(x_values, v_values, "--", label=f"{n_step_list[i]} approx")
+
+			if i == len(n_step_list) - 1:
+				plt.plot(x_values, ux_values, label="exact")
+
+
+
+
+	plt.xlabel('')
+	plt.ylabel('x_values')
+	plt.title(f'Comparison of Numerical Solution and Exact Solution with different n_step')
 	plt.legend()
 	plt.grid(True)
-	plt.savefig(f"plot_problem7_with_{n_step}_steps.pdf")
+	plt.show()
 
-plot_problem7("../problem7_v_x_10steps.txt", 10)
-plot_problem7("../problem7_v_x_100steps.txt", 100)
-plot_problem7("../problem7_v_x_1000steps.txt", 1000)
+filename_list = ["problem7_v_x_10steps.txt", "problem7_v_x_100steps.txt", "problem7_v_x_1000steps.txt"]
+n_step_list = [10, 100, 1000]
+plot_problem7(filename_list, n_step_list)
 
 
 
