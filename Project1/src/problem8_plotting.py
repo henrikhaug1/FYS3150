a598df1fa7d66192c76461d3c4a31d9d0f1e42e6
livelst <livelst@uio.no>
2024-09-11 10:34:09 +0200

    added title in plot 8

--- Project1/src/problem8_plotting.py
+++ Project1/src/problem8_plotting.py
@@ -27,12 +27,12 @@ log10_rel_error_100 = np.log10(relative_error_100)
 log10_abs_error_1000 = np.log10(absolute_error_1000)
 log10_rel_error_1000 = np.log10(relative_error_1000)
 
-fig1, ax1 = plt.subplots(2)
+fig1, ax1 = plt.subplots(2, figsize=(8, 6))
 ax1[0].plot(x_10, log10_abs_error_10, '--', label="n_step=10", alpha=0.5)
 ax1[0].plot(x_100, log10_abs_error_100, '--', label="n_step=100", alpha=0.5)
 ax1[0].plot(x_1000, log10_abs_error_1000, '--', label="n_step=1000", alpha=0.5)
 ax1[0].set_xlabel("x")
-ax1[0].set_ylabel("log10(absolute error)")
+ax1[0].set_ylabel("log10(absolute error)", fontsize="medium")
 ax1[0].legend()
 
 
@@ -40,9 +40,10 @@ ax1[1].plot(x_10, log10_rel_error_10, '--', label="n_step=10", alpha=0.5)
 ax1[1].plot(x_100, log10_rel_error_100, '--', label="n_step=100", alpha=0.5)
 ax1[1].plot(x_1000, log10_rel_error_1000, '--', label="n_step=1000", alpha=0.5)
 ax1[1].set_xlabel("x")
-ax1[1].set_ylabel("log10(relative error)")
+ax1[1].set_ylabel("log10(relative error)", fontsize="medium")
 ax1[1].legend()
 
+ax1[0].set_title("Log10 of absolute- and relative error", fontsize="large")
 fig1.savefig("x_log_error_plot.pdf")
 
 max_rel_err10 = np.max(relative_error_10)
