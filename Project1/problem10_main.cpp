9b2279d512eb9ee075015b18e9a152009758a5d4
livelst <livelst@uio.no>
2024-09-11 12:21:13 +0200

    fixed output formatting problem 10

--- Project1/problem10_main.cpp
+++ Project1/problem10_main.cpp
@@ -8,15 +8,21 @@
 int main(){
 	arma::ivec n_steps = arma::ivec("10 100 1000 10000 100000 1000000");
 
+	int width = 20;
+	int prec = 8;
+
 	std::ofstream ofile;
 	ofile.open("time_comparison.txt");
-	ofile << std::scientific << std::setprecision(15);
-	ofile << "n_steps    Time general (s)     Time special (s)" << "\n"; //making header for file
+
+	ofile << std::left << std::setw(width) << "n_steps" << std::setw(width) << "Time general (s)" << std::setw(width) << "Time special (s)" << std::endl; //making header for file
 
 	for(int i = 0; i < n_steps.size(); i++){
 		double thomas_general_duration_seconds = timer_general(n_steps[i]);
 		double thomas_special_duration_seconds = timer_special(n_steps[i]);
-		ofile << n_steps[i] << "     " << thomas_general_duration_seconds << "      " << thomas_special_duration_seconds << "\n";
+
+		ofile << std::left << std::setw(width) << std::setprecision(prec) << n_steps[i]
+		 	  << std::setw(width) << std::setprecision(prec) << thomas_general_duration_seconds 
+		 	  <<std::setw(width) << std::setprecision(prec) << thomas_special_duration_seconds << std::endl;
 	}
 	return 0;
 }
