636cf76f56eef31b2cf0ca25171b856378843345
Henrik <heh@uio.no>
2024-09-11 10:42:04 +0200

    write problem 10 to file

--- Project1/problem10_main.cpp
+++ Project1/problem10_main.cpp
@@ -1,5 +1,6 @@
 #include <armadillo>
 #include <iostream>
+#include <iomanip>
 #include "problem7_functions.hpp"
 #include "problem9_functions.hpp"
 #include "problem10_functions.hpp"
@@ -7,11 +8,15 @@
 int main(){
 	arma::ivec n_steps = arma::ivec("10 100 1000 10000 100000 1000000");
 
-	std::cout << "n   " <<  "Time general (s)   " << "Time special (s) \n" << std::endl;
+	std::ofstream ofile;
+	ofile.open("time_comparison.txt");
+	ofile << std::scientific << std::setprecision(15);
+	ofile << "n_steps    Time general (s)     Time special (s)" << "\n"; //making header for file
+
 	for(int i = 0; i < n_steps.size(); i++){
 		double thomas_general_duration_seconds = timer_general(n_steps[i]);
 		double thomas_special_duration_seconds = timer_special(n_steps[i]);
-		std::cout << n_steps[i] << "     " << thomas_general_duration_seconds << "      " << thomas_special_duration_seconds << "\n";
+		ofile << n_steps[i] << "     " << thomas_general_duration_seconds << "      " << thomas_special_duration_seconds << "\n";
 	}
 	return 0;
 }
