c8ae5edaa7833f3f3e8172e5e77cbadb1e68758e
Henrik <heh@uio.no>
2024-09-13 12:32:39 +0200

    added header for problem2

---
+++ Project2/include/problem2_functions.hpp
@@ -0,0 +1,12 @@
+#ifndef __problem2_functions_hpp__
+#define __problem2_functions_hpp__
+
+#include <iostream>
+#include <string>
+#include <armadillo>
+
+arma::mat set_up_A_matrix(const int N);
+
+void solve_mat_eq(arma::mat A);
+
+#endif
