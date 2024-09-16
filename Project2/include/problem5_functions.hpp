349c39f41099e11be0a10edfd342263a15a20d11
livelst <livelst@uio.no>
2024-09-16 13:11:40 +0200

    declaired required_transformations_vs_matrix_N() in problem5

--- Project2/include/problem5_functions.hpp
+++ Project2/include/problem5_functions.hpp
@@ -1,5 +1,7 @@
 #ifndef __problem5_functions_hpp__
 #define __problem5_functions_hpp__
 
+void required_transformations_vs_matrix_N();
+
 
 #endif
---
+++ Project2/src/problem5_functions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <armadillo>
+#include "problem5_functions.hpp"
+
+// a) Estimating how the number of required transformations scale with the matrix size N
+void required_transformations_vs_matrix_N(){
+
+	// ...
+
+}
