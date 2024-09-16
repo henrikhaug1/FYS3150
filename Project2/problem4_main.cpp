d17aefcb75ab42119c1a3b09af3c24cde07af4d5
Henrik <heh@uio.no>
2024-09-16 12:41:55 +0200

    small changes

--- Project2/problem4_main.cpp
+++ Project2/problem4_main.cpp
@@ -3,6 +3,9 @@
 #include "problem4_functions.hpp"
 
 int main(){
-	jacobi_test();
+	arma::mat A = set_up_A_matrix(N); //Seting up the tridiagonal matrix
+	arma::vec eigval = solve_eigval(A)
+	arma::mat eigvec = solve_eigvec(A)
+	jacobi_eigensolver(A, 1e-10, eigval, eigvec) // add rest of the arguments
 	return 0;
 }
