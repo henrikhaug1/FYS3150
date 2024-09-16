8c161b2dabf8099418c3839f4f39ac43e23f3af7
livelst <livelst@uio.no>
2024-09-16 12:55:15 +0200

    added ;

--- Project2/problem4_main.cpp
+++ Project2/problem4_main.cpp
@@ -3,9 +3,11 @@
 #include "problem4_functions.hpp"
 
 int main(){
+
 	arma::mat A = set_up_A_matrix(N); //Seting up the tridiagonal matrix
-	arma::vec eigval = solve_eigval(A)
-	arma::mat eigvec = solve_eigvec(A)
+	arma::vec eigval = solve_eigval(A);
+	arma::mat eigvec = solve_eigvec(A);
+
 	jacobi_eigensolver(A, 1e-10, eigval, eigvec) // add rest of the arguments
 	return 0;
 }
