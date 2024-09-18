ca2b81f35d644481317ac0d4cc5c9942784cfc11
Henrik <heh@uio.no>
2024-09-18 16:27:44 +0200

    corrected errors in implementation and cleaned up

--- Project2/problem4_main.cpp
+++ Project2/problem4_main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include "problem3_functions.hpp"
 #include "problem4_functions.hpp"
+#include "problem2_functions.hpp"
 
 int main(){
 
-	arma::mat A = set_up_A_matrix(N); //Seting up the tridiagonal matrix
-	arma::vec eigval = solve_eigval(A);
-	arma::mat eigvec = solve_eigvec(A);
+	//b) Testing jacobi_rotate() with N=6 and ...
+	const int N = 6;
+	arma::mat A = set_up_A_matrix(N);
+	arma::mat R = arma::eye(N, N);
+	int k, l = 0;
 
-	jacobi_eigensolver(A, 1e-10, eigval, eigvec) // add rest of the arguments
+	jacobi_rotate(A, R, k, l);
+
+	std::cout << "A-matrix with eigenvalues along the diagonal" << std::endl;
+	A.print();
+	std::cout << "\n" << std::endl;
+	std::cout << "R-matrix with eigenvectors as columns" << std::endl;
+	R.print();
+	std::cout << "\n" << std::endl;
+
+
 	return 0;
 }
