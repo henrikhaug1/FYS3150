b081af9f47bf0fb6f4884f8f1ac4a8b3b65e5c52
livelst <livelst@uio.no>
2024-09-19 14:43:20 +0200

    commenting changes in problem 3

--- Project2/src/problem3_functions.cpp
+++ Project2/src/problem3_functions.cpp
@@ -4,29 +4,37 @@
 #include "problem3_functions.hpp"
 
 //a)
-double max_offdiag_symmetric(const arma::mat& A, int& k, int& l){ //Takes in an armadillo matrix with references of two integers. 
-// identifies the largest off-diagonal element (in absolute value) in the matrix, 
-// under the assumption of a symmetric matrix
+double max_offdiag_symmetric(const arma::mat& A, int& k, int& l){ 
 
-    int N = A.n_rows; //Gets the size N of an NxN matrix
+    /*This functions takes references to a constant armadillo matrix A, an integer k and an integer l.
+    The function identifies the largest off-diagonal element (in absolute values) of the matrix
+    under the assumption of a symmetric matrix, and returns an integer offDiag */
+
+
+    int N = A.n_rows;
     double offDiag = 0.0;
-    for (int i = 0; i < N; ++i) {//Loops over all values of N (size of A)
-        for (int j = 0; j < N; ++j){ //Again. LÖÖPS over all values in A
-            if (i != j){ //Ignores the diagonal
-                if (fabs(offDiag) < fabs(A(i, j))){ //Checks if the current offDiagonal value of A is larger than a previous one (absolute values)
-                    offDiag = A(i, j); //New largest offDiagonal value found
-                    k = i; //The integer references are updated for this value
+
+    for (int i = 0; i < N; ++i){
+
+        for (int j = 0; j < N; ++j){
+
+            if (i != j){                                //Ignores the diagonal
+                if (fabs(offDiag) < fabs(A(i, j))){     //Checks if the current offDiagonal value of A is larger than a previous one (absolute values)
+                    offDiag = A(i, j);                  //New largest offDiagonal value found
+                    k = i;
                     l = j;
                 }
             }
         }
     }
-// writes the matrix indices for this element to the two integer references
-    return offDiag; //Returns the largest off diagonal value with corresponding integer references
+    return offDiag;
 }
 
 //b)
-void test_max_offdiag_symmetric(){ //Test code with a predefined matrix
+void test_max_offdiag_symmetric(){
+
+
+    /* This function tests the function max_offdiag_symmetric() with a predefined matrix.*/ 
 
     int N = 4; 
     arma::mat A = arma::eye(N, N);
@@ -36,11 +44,16 @@ void test_max_offdiag_symmetric(){ //Test code with a predefined matrix
     A(1,2) = -0.7;
     A(0,3) = 0.5;
 
-    double offDiag_returned = 0.0; //Defining variables 
+    double offDiag_returned = 0.0; 
     int l;
     int k;
 
     offDiag_returned = max_offdiag_symmetric(A, l, k); 
+
+    A.print("A-matrix:");
+    std::cout << "\n";
+
     std::cout << "Largest off-diagonal value in A: " << offDiag_returned << " (Absolute values) " << std::endl;
-    std::cout << "k:" << k << " l:" << l << std::endl;
+
+    std::cout << "k: " << k << ", " << " l: " << l << std::endl;
 }
