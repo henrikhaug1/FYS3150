68e11e7833dafaca65edb2cb3bfe6693e4dd4b24
livelst <livelst@uio.no>
2024-09-23 12:09:06 +0200

    docstring changes

--- Project2/problem4_main.cpp
+++ Project2/problem4_main.cpp
@@ -5,7 +5,8 @@
 
 int main(){
 
-	//b) Testing jacobi_rotate() with N=6 and comparing numerical and analytical results
+	/*b) Testing jacobi_rotate() with N=6 and comparing numerical and analytical results
+	by printing to terminal.*/
 
 	test_jacobi_rotate();
 
