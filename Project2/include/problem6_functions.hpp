f75e0072f906e08346088aa9a40816e21f9a9d6e
Henrik Haug <heh@ulrik.uio.no>
2024-09-23 13:47:15 +0200

    added sorting algorithm to header

--- Project2/include/problem6_functions.hpp
+++ Project2/include/problem6_functions.hpp
@@ -1,5 +1,8 @@
 #ifndef __problem6_functions_hpp__
 #define __problem6_functions_hpp__
+#include <armadillo>
 
-void compare_write_to_file();
+void compare_write_to_file(const int N);
+
+void custom_selection_sort(arma::vec& arr, int& first_index, int& second_index, int& third_index);
 #endif
