ed2a861d265d30d91c5c33006b8ae26d25395419
livelst <livelst@uio.no>
2024-09-23 16:25:45 +0200

    n as commandline argument in problem 6

--- Project2/problem6_main.cpp
+++ Project2/problem6_main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include "problem6_functions.hpp"
 
-int main(){
-	int N = 10;
+int main(int argc, char* argv[]){
+
+    if(argc != 2){
+        std::cout << "Provide an integer N for NxN-matrix! "<< std::endl;
+        std::cout << "Example: " << argv[0] << " 6" << std::endl;
+        return 1;
+    }
+
+    int N = atoi(argv[1]);
+    
 	compare_write_to_file(N);
 	
 	return 0;
