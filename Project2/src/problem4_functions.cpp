eb573ed3745f39211c60bb4e414a8743ef23d6cb
livelst <livelst@uio.no>
2024-09-23 13:33:33 +0200

    fixed jacobi_rotate:))))))))))))

--- Project2/src/problem4_functions.cpp
+++ Project2/src/problem4_functions.cpp
@@ -8,184 +8,131 @@
 // a)
 void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l, int& iterations){
 
-    /* This function takes references to an armadillo matrix A and an armadillo matrix R, and two integers as arguments.
-    The functions is an implementation of Jacobi´s rotation method.*/
-    
     //Step 1)
-    double eps = 1.0e-8; //Tolerance
+    double eps = 1.0e-8;  // Tolerance
     int row = A.n_rows;
-    int col = A.n_cols;
 
     //Step 2) finding k and l 
     double offDiag = max_offdiag_symmetric(A, k, l);
-
-    double a_kl = A(k, l); //Initial, needed to start the while loop
+    double a_kl = A(k, l);  // Initial value to start the loop
 
     //Step 3)
     while(fabs(a_kl) > eps){
-        a_kl = A(k, l);  //First and continuing a_xx
-        double a_ll = A(l, l);  //When continued, will get the a_xx_mp1 calculated in step 3.3
+
+        a_kl = A(k, l);
+        double a_ll = A(l, l);
         double a_kk = A(k, k);
 
         //Step 3.1)
-        double tau = (a_ll - a_kk) / (2 * a_kl);  
-        
-        //Step 3.2)
+        double tau = (a_ll - a_kk) / (2 * a_kl);
+
+        //Step 3.2) Choose the solution that gives the smallest tθ
         double t_theta;
 
-        if(tau > 0){ //Choose the solution that gives the smallest tθ. Step 3.2)
+        if(tau > 0){
             t_theta = 1 / (tau + sqrt(1 + tau * tau));
-        } 
+        }
 
-        else if(tau < 0){
+        else{
             t_theta = 1 / (tau - sqrt(1 + tau * tau));
-        } 
-        
+        }
+
         //Step 3.3
         double c_theta = 1 / sqrt(1 + t_theta * t_theta);
         double s_theta = c_theta * t_theta;
 
-        
-        double a_kk_mp1;
-        a_kk_mp1 = a_kk * (c_theta * c_theta) - ( 2 * a_kl * (c_theta * s_theta) ) + a_ll * (s_theta * s_theta);  //Calculating next a_xx
-        A(k, k) = a_kk_mp1; //Updating A
+        double a_kk_mp1 = a_kk * (c_theta * c_theta) - (2 * a_kl * c_theta * s_theta) + a_ll * (s_theta * s_theta);
+        double a_ll_mp1 = a_ll * (c_theta * c_theta) + (2 * a_kl * c_theta * s_theta) + a_kk * (s_theta * s_theta);
 
-        double a_ll_mp1;
-        a_ll_mp1 = a_ll * (c_theta * c_theta) + ( 2 * a_kl * (c_theta * s_theta) ) + a_kk * (s_theta * s_theta);  //Repeat
+        A(k, k) = a_kk_mp1;
         A(l, l) = a_ll_mp1;
-
-        double a_kl_mp1;
-        a_kl_mp1 = 0.0;
-        A(k, l) = a_kl_mp1;
-
-        double a_lk_mp1;
-        a_lk_mp1 = 0.0;
-        A(l, k) = a_lk_mp1;
+        A(k, l) = 0.0;
+        A(l, k) = 0.0;
 
         // Updating all other elements where i!=k and i!=l
-        for (int i = 0; i < row; ++i){
+        for(int i = 0; i < row; ++i){
 
-
-            if(i != k and i != l){
+            if(i != k && i != l){
 
                 double a_ik = A(i, k);
                 double a_il = A(i, l);
 
-                //A[i][k]
-                double a_ik_mp1;
-                a_ik_mp1 = (a_ik * c_theta) - (a_il * s_theta);
-                A(i, k) = a_ik_mp1;
-
-                //A[k][i]
-                double a_ki_mp1;
-                a_ki_mp1 = a_ik_mp1;
-                A(k, i) = a_ki_mp1;
-
-                //A[i][l]
-                double a_il_mp1;
-                a_il_mp1 = (a_il * c_theta) + (a_ik * s_theta);
-                A(i, l) = a_il_mp1;
-
-                //A[l][i]
-                double a_li_mp1;
-                a_li_mp1 = a_il_mp1;
-                A(l, i) = a_li_mp1;
+                A(i, k) = a_ik * c_theta - a_il * s_theta;
+                A(k, i) = A(i, k);
+                A(i, l) = a_il * c_theta + a_ik * s_theta;
+                A(l, i) = A(i, l);
             }
 
             //Step 3.4
             double r_ik = R(i, k);
             double r_il = R(i, l);
 
-            //R[i][k]
-            double r_ik_mp1;
-            r_ik_mp1 = (r_ik * c_theta) - (r_il * s_theta);
-            R(i, k) = r_ik_mp1;
-
-            //R[i][l]
-            double r_il_mp1;
-            r_il_mp1 = (r_il * c_theta) + (r_ik * s_theta);
-            R(i, l) = r_il_mp1;
-
-            iterations = iterations + 1;
+            R(i, k) = r_ik * c_theta - r_il * s_theta;
+            R(i, l) = r_il * c_theta + r_ik * s_theta;
         }
 
         //Step 3.5
-        double offDiag = max_offdiag_symmetric(A, k, l); 
-
-        
+        offDiag = max_offdiag_symmetric(A, k, l);
     }
+    iterations += 1;
 }
 
-
-//b)
 void test_jacobi_rotate(){
-
-    /* This function tests the function jacobi_rotate() with a 6x6-matrix.*/
 
     int N = 6;
     arma::mat A = set_up_A_matrix(N);
     arma::mat R = arma::eye(N, N);
-    int k = 0;
-    int l = 0;
-    int iterations;
+
+    int k, l;
+    int iterations = 0;
 
     jacobi_rotate(A, R, k, l, iterations);
 
     arma::vec numerical_eigval = arma::vec(A.n_rows);
     for(int i = 0; i < A.n_rows; i++){
-        numerical_eigval[i] = A.col(i)[i];
+
+        numerical_eigval[i] = A(i, i);
     }
 
-    A.print("A-matrix after Jacobi");
-    std::cout << "\n";
-    R.print("R-matrix after Jacobi");
-    std::cout << "\n";
-
-
-    //Analytical solution
+    // Analytical solution
     arma::mat analytical_eigvec(N, N);
     arma::vec analytical_eigval(N);
     analytical_eig_vec_val(analytical_eigvec, analytical_eigval, N);
 
-    //Printing analytical solution
+    // Printing analytical solution
     analytical_eigval.print("Eigenvalues analytical:");
     std::cout << "\n";
     analytical_eigvec.print("Eigenvectors analytical:");
     std::cout << "\n";
 
-    //Printing numerical solution
+    // Printing numerical solution
     numerical_eigval.print("Eigenvalues numerical:");
     std::cout << "\n";
     R.print("Eigenvectors numerical:");
     std::cout << "\n";
 
-    //Differences between anaylical and numerical eigvalues and eigvectors
-    arma::vec eigval_diff = numerical_eigval - analytical_eigval;                  //Calculates difference in eigenvalues
-    arma::mat eigvec_diff(N, N);                                 //Calculates difference in eigenvectors
+    // Differences between analytical and numerical eigenvalues and eigenvectors
+    arma::vec eigval_diff = numerical_eigval - analytical_eigval; // Calculates difference in eigenvalues
+    arma::mat eigvec_diff(N, N);
 
-    //Finds the signs, ref sign differences in eigenvecs 2 and 6
-    arma::mat analytical_eigvec_sign = sign(analytical_eigvec); 
+    arma::mat analytical_eigvec_sign = sign(analytical_eigvec);
     arma::mat numerical_eigvec_sign = sign(R);
 
-    //Loops over all eigenvectors
     for(int i = 0; i < N; ++i){
 
-        if(analytical_eigvec_sign.col(i)[0] == numerical_eigvec_sign.col(i)[0]){   //Checks if the signs are in agreement
+        if(analytical_eigvec_sign.col(i)[0] == numerical_eigvec_sign.col(i)[0]){ //Checks if the signs are in agreement
             
-            eigvec_diff.col(i) = analytical_eigvec.col(i) - R.col(i);       //If they are; Subtract
+            eigvec_diff.col(i) = analytical_eigvec.col(i) - R.col(i);
         }
+        else{ //If they are not; Double negative
 
-        else{
-            
-            eigvec_diff.col(i) = analytical_eigvec.col(i) + R.col(i);       //If they are not; Double negative
+            eigvec_diff.col(i) = analytical_eigvec.col(i) + R.col(i);
         }
     }
 
-    //Printing the difference between numerical and analytical eigenvalues and eigenvectors
+    // Printing the difference between numerical and analytical eigenvalues and eigenvectors
     eigval_diff.print("Difference between numerical and analytical eigenvalues:");
     std::cout << "\n";
-
     eigvec_diff.print("Difference between numerical and analytical eigenvectors:");
-
 }
 
