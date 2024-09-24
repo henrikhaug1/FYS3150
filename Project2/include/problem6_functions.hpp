426633dcfc6d9df38d93d06e8aec3769b7fed785
Henrik Haug <heh@ulrik.uio.no>
2024-09-24 15:13:41 +0200

    added new function to hpp

--- Project2/include/problem6_functions.hpp
+++ Project2/include/problem6_functions.hpp
@@ -2,7 +2,9 @@
 #define __problem6_functions_hpp__
 #include <armadillo>
 
-void compare_write_to_file(const int N);
+void write_to_file_numerical(const int N);
+
+void write_to_file_analytical();
 
 void custom_selection_sort(arma::vec& arr, int& first_index, int& second_index, int& third_index);
 #endif
