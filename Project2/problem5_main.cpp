8576d797a70ad43c5966802fdbe03a712b10e322
livelst <livelst@uio.no>
2024-09-25 13:51:03 +0200

    some changes

--- Project2/problem5_main.cpp
+++ Project2/problem5_main.cpp
@@ -2,47 +2,47 @@
 #include <armadillo>
 #include <string>
 #include <iomanip>
+#include <ctime> 
+#include <fstream> 
 #include "problem2_functions.hpp"
 #include "problem3_functions.hpp"
 #include "problem4_functions.hpp"
 #include "problem5_functions.hpp"
 
-int main(){
+int main() {
+    // Seed the random number generator with the current time
+    arma::arma_rng::set_seed(std::time(nullptr)); // Set the seed for random number generation
 
-	//a)
-	double eps = 1.0e-8;
-	arma::vec eigenvalues;
-	arma::mat eigenvectors;
-	const int maxiter = 10000;
-	int iterations;
-	bool converged;
-	arma::vec N = {5, 10, 15};
+    // a)
+    double eps = 1.0e-8;
+    arma::vec eigenvalues;
+    arma::mat eigenvectors;
+    const int maxiter = 10000;
+    int iterations;
+    bool converged;
+    arma::vec N = {5, 10, 15};
 
-	int width = 20;
+    int width = 20;
 
-	std::string filename = "N_vs_similarity_transformation.txt";
-	std::ofstream ofile;
-	ofile.open(filename);
-	ofile << std::left << std::setw(width) << "N" << std::setw(width) << "similarity transformation" << std::endl;
+    std::string filename = "N_vs_similarity_transformation.txt";
+    std::ofstream ofile;
+    ofile.open(filename);
+    ofile << std::left << std::setw(width) << "N" << std::setw(width) << "similarity transformation" << std::endl;
 
-	for(int i = 0; i < N.size(); ++i){
-		arma::mat A = set_up_A_matrix(N[i]);
-		jacobi_eigensolver(A, eps, eigenvalues, eigenvectors, maxiter, iterations, converged);
+    for (int i = 0; i < N.size(); ++i) {
+        arma::mat A = set_up_A_matrix(N[i]);
 
-		ofile << std::left << std::setw(width) << N[i] << std::setw(width) << iterations << std::endl; //making header for file
+        jacobi_eigensolver(A, eps, eigenvalues, eigenvectors, maxiter, iterations, converged);
 
-	}
+        // Printing
+        A.print();
+        std::cout << "\n";
 
-	ofile.close();
+        ofile << std::left << std::setw(width) << N[i] << std::setw(width) << iterations << std::endl; // Making header for file
+    }
 
-	//b)
-	// Generate random N*N matrix
-	arma::mat A_dense = arma::randu<arma::mat>(10, 10);
+    ofile.close();
+    
 
-	// Symmetrize the matrix by reflecting the upper triangle to lower triangle
-	A_dense = arma::symmatu(A_dense); 
-
-	jacobi_eigensolver(A_dense, eps, eigenvalues, eigenvectors, maxiter, iterations, converged);
-
-	return 0;
+    return 0;
 }
