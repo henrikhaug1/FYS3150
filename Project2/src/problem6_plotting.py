ff27ada812921edda99683ecaaaefcfec0c298a6
Henrik Haug <heh@ulrik.uio.no>
2024-09-25 11:10:35 +0200

    changed plotting completely - analytical now works, but numerical is still weird for high N values

--- Project2/src/problem6_plotting.py
+++ Project2/src/problem6_plotting.py
@@ -1,33 +1,32 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Load numerical and analytical eigenvectors
+n = 100
+N = n + 1
+h = 1/n
+a = -1/(h**2)
+d = 2/(h**2)
+x_hat_analytical = np.linspace(0, 1, n + 1)
+
 numerical_eigvecs = np.loadtxt("numerical_smallest_lambda.txt", usecols=(0, 1, 2), skiprows=1)
-analytical_eigvecs = np.loadtxt("analytical_smallest_lambda.txt", usecols=(0, 1, 2), skiprows=1)
-
-# Assuming x-coordinates are uniformly spaced between 0 and 1
 N_numerical = numerical_eigvecs.shape[0]
-N_analytical = analytical_eigvecs.shape[0]
 x_hat_numerical = np.linspace(0, 1, N_numerical)
-x_hat_analytical = np.linspace(0, 1, N_analytical)
 
-# Plotting
-fig, ax = plt.subplots(3, figsize=(10, 8))
+#multiplying eigenvector 3 with -1 to get the same shape as numerical plot.
+for i in range(len(numerical_eigvecs)):
+	numerical_eigvecs[i][2] = -1 * numerical_eigvecs[i][2]
 
-# Plot each eigenvector
 for i in range(3):
 	#setting boundary points
 	numerical_eigvecs[0, i] = 0
 	numerical_eigvecs[-1, i] = 0
 
-
-	ax[i].plot(x_hat_numerical, numerical_eigvecs[:, i], label=f"Numerical Eigenvector {i+1}", color='magenta')
-	ax[i].plot(x_hat_analytical, analytical_eigvecs[:, i], label=f"Analytical Eigenvector {i+1}", linestyle = "--", color='blue')
-	ax[i].set_xlabel("x_hat")
-	ax[i].set_ylabel("Vector")
-	ax[i].set_title(f"Eigenvector Comparison {i+1}")
-	ax[i].legend()
-	ax[i].grid()
-
+fig1, ax1 = plt.subplots(3, 1, figsize=(8, 6))
+for i in range(3):
+	analytical_solution = np.sin((i + 1) * np.pi * x_hat_analytical) if i != 2 else -np.sin((i + 1) * np.pi * x_hat_analytical)
+	ax1[i].plot(x_hat_analytical, analytical_solution, label="Analytical", color="red")
+	ax1[i].plot(x_hat_numerical, numerical_eigvecs[:, i], label=f"Numerical Eigenvector {i+1}",linestyle="--", marker="x", color='blue')
+	ax1[i].legend()
+	ax1[i].grid(True)
 plt.tight_layout()
 plt.show()
