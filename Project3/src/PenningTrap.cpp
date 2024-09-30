22e7e070cbb34507a1fdc7b9aa0f148935f644c8
livelst <livelst@uio.no>
2024-09-30 11:05:55 +0200

    function declarations for PenningTrap and scelleton for PenningTrap.cpp

--- Project3/src/PenningTrap.cpp
+++ Project3/src/PenningTrap.cpp
@@ -1,12 +1,67 @@
 #include <iostream>
 #include <armadillo>
+
 #include "PenningTrap.hpp"
 
-PenningTrap::PenningTrap(arma::vec external_electric_field, arma::vec external_magnetic_field, 
-		arma::vec force_interaction)
+// Constructor
+PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in)
 {
-	external_electric_field = external_electric_field_in;
-	external_magnetic_field = external_magnetic_field_in;
-	force_interaction = froce_interaction_in;
-
+	// ....
 }
+
+// Add a particle to the trap
+void PenningTrap::add_particle(Particle p_in)
+{
+	// ...
+}
+
+// External electric field at point r=(x,y,z)
+arma::vec PenningTrap::external_E_field(arma::vec r)
+{
+	// ...
+} 
+
+// External magnetic field at point r=(x,y,z)
+arma::vec PenningTrap::external_B_field(arma::vec r)
+{
+	// ...
+}  
+
+// Force on particle_i from particle_j
+arma::vec PenningTrap::force_particle(int i, int j)
+{
+	// ...
+}
+
+// The total force on particle_i from the external fields
+arma::vec PenningTrap::total_force_external(int i)
+{
+	// ...
+}
+
+// The total force on particle_i from the other particles
+arma::vec PenningTrap::total_force_particles(int i)
+{
+	// ...
+}
+
+// The total force on particle_i from both external fields and other particles
+arma::vec PenningTrap::total_force(int i)
+{
+	// ...
+}
+
+// Evolve the system one time step (dt) using Runge-Kutta 4th order
+void PenningTrap::evolve_RK4(double dt)
+{
+	// ...
+}
+
+// Evolve the system one time step (dt) using Forward Euler
+void PenningTrap::volve_forward_Euler(double dt)
+{
+	// ...
+}
+
+
+
