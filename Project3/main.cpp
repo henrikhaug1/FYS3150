012125bb742958f6a2f47212516df2d74de6e53a
Henrik Haug <heh@ulrik.uio.no>
2024-10-04 13:04:50 +0200

    small changes

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -3,4 +3,6 @@
 #include "Particle.hpp"
 #include "PenningTrap.hpp"
 
-Particle particle = Particle()
+arma::vec position1 = arma::vec({20, 0, 20});
+arma::vec velocity1 = arma::vec({0, 40, 5});
+Particle particle = Particle(charge_in=1.0, mass_in=40.078 position_in=position1, velocity_in=velocity1)
