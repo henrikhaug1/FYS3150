a606008e6f37d0657467b79ef96ea93b31a16492
Henrik Haug <heh@ulrik.uio.no>
2024-10-05 14:43:04 +0200

    added solver functoins

---
+++ Project3/include/ForwardEulerSolver.hpp
@@ -0,0 +1,18 @@
+#ifndef __ForwardEulerSolver_hpp__
+#define __ForwardEulerSolver_hpp__
+
+#include "Particle.hpp"
+#include "PenningTrap.hpp"
+
+class ForwardEulerSolver
+{
+	public:
+		arma::vec nitial_conditions;
+
+		ForwardEulerSolver(arma::vec U0_in)
+		{
+
+		}
+}
+
+#endif
