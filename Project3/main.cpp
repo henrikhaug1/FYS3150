299a9da544d725d7b66db91fff4fb0a8b92f6dff
Henrik Haug <heh@ulrik.uio.no>
2024-10-11 10:09:57 +0200

    added pretty prints

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -13,15 +13,31 @@ int main()
 	arma::vec velocity2 = arma::vec({0, 40, 5});
 	Particle particle2 = Particle(1.0, 40.078, position2, velocity2);
 
-	std::cout << "mass : " << particle1.mass << " charge: " << particle1.charge << " velocity: "<< particle1.velocity << " position: " << particle1.position << "\n";
-	std::cout << "mass : " << particle2.mass << " charge: " << particle2.charge << " velocity: "<< particle2.velocity << " position: " << particle2.position << "\n";
+	//std::cout << "mass : " << particle1.mass << " charge: " << particle1.charge << " velocity: "<< particle1.velocity << " position: " << particle1.position << "\n";
+	//std::cout << "mass : " << particle2.mass << " charge: " << particle2.charge << " velocity: "<< particle2.velocity << " position: " << particle2.position << "\n";
 	
 	PenningTrap trap = PenningTrap();
 	trap.add_particle(particle1);
 	trap.add_particle(particle2);
+	trap.external_E_field(position1);
 
-	trap.evolve_forward_Euler(0.01);
-	std::cout << particle1.position << " " << particle1.velocity << std::endl;
-	std::cout << particle2.position << " " << particle2.velocity << std::endl;
+	std::cout << "--------- PARTICLE 1 ---------" << std::endl;
+	std::cout << "Position: \n" << trap.external_E_field(position1) << std::endl;
+	std::cout << "\n";
+	std::cout << "Velocity: \n" << trap.external_E_field(position1) << std::endl; 
+	std::cout << "\n";
+	std::cout << "External electric field: \n" << trap.external_E_field(position1) << std::endl;
+	std::cout << "\n";
+	std::cout << "External magnetic field: \n" << trap.external_B_field(position1) << std::endl;
+	std::cout << "\n";
+	std::cout << "Force on particle i from particle j\n: " << trap.force_particle(0, 1) << std::endl;
+	std::cout << "\n";
+	std::cout << "The total force on particle i from the external fields: " << trap.total_force_external(0) << std::endl;
+	std::cout << "\n";
+	std::cout << "TThe total force on particle i from the other particles: " << trap.total_force_external(0) << std::endl;
+
+	//trap.evolve_forward_Euler(0.01);
+	//std::cout << particle1.position << " " << particle1.velocity << std::endl;
+	//std::cout << particle2.position << " " << particle2.velocity << std::endl;
 
 }
