d8bee149f6980c18958df10e3d7b21f215ac9002
Henrik Haug <heh@ulrik.uio.no>
2024-10-11 15:59:38 +0200

    added fancier plotting :D

--- Project3/src/plotting.py
+++ Project3/src/plotting.py
@@ -3,5 +3,11 @@ import matplotlib.pyplot as plt
 
 time, z_pos = np.loadtxt("simulation_data_1_particle.txt", unpack=True)
 
-plt.plot(time, z_pos)
+plt.plot(time, z_pos, label = "z - position over 50 microseconds", color = "magenta")
+plt.title("movement of a single particle in Penning trap using RK4")
+plt.xlabel("time in microseconds")
+plt.ylabel("z-values")
+plt.grid(True)
+plt.tight_layout()
+plt.legend()
 plt.show()
