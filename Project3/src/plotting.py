cc86461f807706a273c7c9cfebe65ad20c054bf6
Henrik Haug <heh@ulrik.uio.no>
2024-10-13 16:51:00 +0200

    added more plotting

--- Project3/src/plotting.py
+++ Project3/src/plotting.py
@@ -1,12 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-time, z_pos = np.loadtxt("simulation_data_1_particle.txt", unpack=True)
+time_z, z_pos = np.loadtxt("specific_analytical_z.txt", unpack=True)
 
-plt.plot(time, z_pos, label = "z - position over 50 microseconds", color = "magenta")
+
+plt.plot(time_z, z_pos, label = "z - position over 50 microseconds", color = "magenta")
 plt.title("movement of a single particle in Penning trap using RK4")
 plt.xlabel("time in microseconds")
-plt.ylabel("z-values")
+plt.ylabel("z(t)")
+plt.grid(True)
+plt.tight_layout()
+plt.legend()
+plt.show()
+
+x, y = np.loadtxt("specific_analytical_xy.txt", unpack=True)
+
+
+plt.plot(x, y, label = "z - position over 50 microseconds", color = "magenta")
+plt.title("movement of a single particle in Penning trap using RK4")
+plt.xlabel("time in microseconds")
+plt.ylabel("f(t)")
 plt.grid(True)
 plt.tight_layout()
 plt.legend()
