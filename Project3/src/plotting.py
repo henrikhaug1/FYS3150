27529c180d5545c4763613e39851c20dfe39fe13
Henrik Haug <heh@ulrik.uio.no>
2024-10-14 15:38:01 +0200

    edited plotting

--- Project3/src/plotting.py
+++ Project3/src/plotting.py
@@ -13,10 +13,21 @@ plt.tight_layout()
 plt.legend()
 plt.show()
 
-x, y = np.loadtxt("specific_analytical_xy.txt", unpack=True)
+x1, y1 = np.loadtxt("specific_analytical_xy_particle1.txt", unpack=True)
 
 
-plt.plot(x, y, label = "z - position over 50 microseconds", color = "magenta")
+
+plt.plot(x1, y1, label = "z - position over 50 microseconds", color = "blue")
+plt.title("movement of a single particle in Penning trap using RK4")
+plt.xlabel("time in microseconds")
+plt.ylabel("f(t)")
+plt.grid(True)
+plt.tight_layout()
+plt.legend()
+plt.show()
+
+x2, y2 = np.loadtxt("specific_analytical_xy_particle2.txt", unpack=True)
+plt.plot(x2, y2, label = "z - position over 50 microseconds", color = "red")
 plt.title("movement of a single particle in Penning trap using RK4")
 plt.xlabel("time in microseconds")
 plt.ylabel("f(t)")
